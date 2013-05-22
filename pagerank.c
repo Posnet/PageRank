252aa591ac7080c05d70f3d1d1f750464ddc10c1
Alec Posney <alec.network@gmail.com>
2013-05-22 10:25:24 +1000

    initial commit

--- pagerank.c
+++ pagerank.c
@@ -20,49 +20,8 @@ typedef struct Node Node;
 ////////////////////////
 
 /* pagerank.c */
-SuperPage *SuperPage_create(page *p);
-Node *Node_create(void);
-Node *List_create(void);
-Node *insert_after(Node *n, int index);
-Node *delete_node(Node *n);
-Node *get_next(void);
-void *scalloc(size_t num, size_t size);
-void *smalloc(size_t size);
-void SuperPage_free(SuperPage *p);
-void Free_List(Node *head);
-void reset_next(void);
-void init_globals(int ncores, int npages, int nedges, double dampener);
-void process_data(list *plist);
-void process_node(Node *n);
-void tick(void);
-void free_all(void);
-void print_nodes(list *plist);
-void edge_cases(list *plist, int ncores, int nedges);
-void pagerank(list *plist, int ncores, int npages, int nedges, double dampener);
-double diff_squared(double curr, double prev);
-int main(void);
-int check_convergance(void);
 
 
-//////////////////////
-//Stuct Definitions //
-//////////////////////
-
-struct SuperPage
-{
-    int index;
-    int noutlinks;
-    double partialRank;
-    Node *inlinks;
-};
-
-struct Node
-{
-    Node *prev;
-    Node *next;
-    int type; //  tail <  0  = normal head > 0
-    int elem;
-};
 
 /////////////////////////////
 //GENERIC HELPER FUNCTIONS //
@@ -112,137 +71,9 @@ double diff_squared(double curr, double prev)
     return temp * temp;
 }
 
-/**
- * Initialises and Allocated memory for a new
- * Superpage
- * @param  p the page being copied
- * @return   new super page pointer
- */
-SuperPage *SuperPage_create(page *p)
-{
-    SuperPage *newPage = (SuperPage *)smalloc(sizeof(SuperPage));
-    newPage->index = p->index;
-    newPage->noutlinks = p->noutlinks;
-    newPage->partialRank = 0;
-    newPage->inlinks = NULL;
-    return newPage;
-}
-
-/**
- * frees a given SuperPage's memory
- * @param p the page to be freed
- */
-void SuperPage_free(SuperPage *p)
-{
-    if (p)
-    {
-        Free_List(p->inlinks);
-        free(p);
-    }
-}
 
 
-//////////////////////////////
-//Linked List Access Methods//
-//////////////////////////////
 
-/**
- * Initialises and allocates
- * a new Node
- * @param  void
- * @return      new Node pointer
- */
-Node *Node_create(void)
-{
-    Node *n = (Node *)smalloc(sizeof(Node));
-    n->elem = -1;
-    n->next = NULL;
-    n->prev = NULL;
-    n->type = 0;
-    return n;
-}
-
-/**
- * Creates a new doubly linked list
- * @param  void
- * @return      returns the new virtual head node
- */
-Node *List_create(void)
-{
-    Node *head = Node_create();
-    head->type = 1;
-    return head;
-}
-
-/**
- * Inserts a node after a given node
- * @param  n     Node to be inserted after
- * @param  index the elem value of the new Node
- * @return       returns a pointer to the new Node
- */
-Node *insert_after(Node *n, int index)
-{
-    Node *newNode = Node_create();
-    newNode->elem = index;
-    if (n)
-    {
-        if (n->next)
-        {
-            n->next->prev = newNode;
-            newNode->next = n->next;
-            n->next = newNode;
-            newNode->prev = n;
-        }
-        else
-        {
-            n->next = newNode;
-            newNode->prev = n;
-        }
-    }
-    return newNode;
-}
-
-/**
- * Deletes a given Node and frees it
- * @param  n Node to be deleted
- * @return   returns the Node after the one being
- * deleted, if at the end of the list returns NULL
- * If the node is the head of the list return NULL
- */
-Node *delete_node(Node *n)
-{
-    if (n->type)
-    {
-        return NULL;
-    }
-    Node *res = NULL;
-    n->prev->next = n->next;
-    if (n->next)
-    {
-        res = n->next;
-        n->next->prev = n->prev;
-    }
-    free(n);
-    return res;
-}
-
-/**
- * Frees all the nodes and the virtual head
- * of a given list
- * @param head head of list to be freed
- */
-void Free_List(Node *head)
-{
-    Node *n = head->next;
-    Node *t = NULL;
-    while (n)
-    {
-        t = n->next;
-        free(n);
-        n = t;
-    }
-    free(head);
-}
 
 ///////////////////////////////
 //PAGE RANK GLOBAL VARIABLES //
@@ -260,11 +91,6 @@ double jumpProb;
 double baseProb;
 double epsilon = EPSILON * EPSILON;
 double norm;
-
-
-SuperPage **superPages;
-Node *Head;
-Node *globalCurr;
 
 
 ///////////////////////////////
@@ -311,7 +137,6 @@ void init_globals(int ncores, int npages, int nedges, double dampener)
     pages = npages;
     cores = ncores;
     edges = nedges;
-    Head = List_create();
     jumpProb = ((1.0 - damp) / pages);
     baseProb = 1.0 / npages;
 
@@ -319,7 +144,6 @@ void init_globals(int ncores, int npages, int nedges, double dampener)
     PrevRank = (double *)smalloc(sizeof(double) * pages);
     TempRank = NULL;
     hasConverged = (int *)smalloc(sizeof(int) * pages);
-    superPages = (SuperPage **)smalloc(sizeof(SuperPage *)*pages);
     norm = 0;
 }
 
@@ -332,47 +156,7 @@ void init_globals(int ncores, int npages, int nedges, double dampener)
  */
 void process_data(list *plist)
 {
-    norm = 0;
-    Node *current = Head;
-    Node *ptr;
-    node *curr = plist->head;
-    node *c;
-    int index;
-    double rank;
-    while (curr)
-    {
-        index = curr->page->index;
-        if (curr->page->inlinks)  // Not dangling page
-        {
-            SuperPage *newPage = SuperPage_create(curr->page);
-            superPages[index] = newPage;
-            current = insert_after(current, index);
-            newPage->inlinks = List_create();
-            ptr = newPage->inlinks;
-            c = curr->page->inlinks->head;
-            rank = 0;
-            while (c)
-            {
-                insert_after(ptr, c->page->index); //Possible remove has converged here
-                rank += (baseProb / c->page->noutlinks);
-                c = c->next;
-            }
-            rank = jumpProb + (damp * rank);
-            PageRank[index] = rank;
-            PrevRank[index] = rank;
-            hasConverged[index] = 0;
-            norm += diff_squared(baseProb, rank);
-        }
-        else   // Dangling page
-        {
-            PageRank[index] = jumpProb;
-            PrevRank[index] = jumpProb;
-            superPages[index] = NULL;
-            hasConverged[index] = curr->page->noutlinks;
-            norm += diff_squared(baseProb, jumpProb);
-        }
-        curr = curr->next;
-    }
+
 }
 
 /**
@@ -386,35 +170,7 @@ void process_data(list *plist)
  */
 void process_node(Node *n)
 {
-    SuperPage *p = superPages[n->elem];
-    double rank = 0;
-    int index;
-    Node *curr = p->inlinks->next;
-    while (curr)
-    {
-        index = curr->elem;
-        if (hasConverged[index])
-        {
-            p->partialRank += PrevRank[index]/hasConverged[index];
-            curr = delete_node(curr);
-        }
-        else
-        {
-            rank += (PrevRank[index] / superPages[index]->noutlinks);
-            curr = curr->next;
-        }
-    }
-    index = n->elem;
-    rank += p->partialRank;
-    rank = jumpProb + (damp * rank);
-    PageRank[index] = rank;
-    norm += diff_squared(rank, PrevRank[index]);
-    if (!(p->inlinks->next))
-    {
-        hasConverged[index] = (rank/p->noutlinks);
-        PrevRank[index] = rank;
-        delete_node(n);
-    }
+
 }
 
 /**
